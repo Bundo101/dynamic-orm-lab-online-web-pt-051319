f85cb783ace2e652faaa0c02ff27c8f8109d7577
James Britton <brittonjp@gmail.com>
2019-08-02 17:22:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,27 +4,5 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
-  self.column_names.each do |col_name|
-    attr_accessor col_name.to_sym
-  end
-
-  def self.table_name
-    self.to_s.downcase.pluralize
-  end
-
-  def self.column_names
-    DB[:conn].results_as_hash = true
-
-    sql = "pragma table_info('#{table_name}')"
-
-    table_info = DB[:conn].execute(sql)
-    column_names = []
-    table_info.each do |row|
-      column_names << row["name"]
-    end
-    column_names.compact
-  end
-
-
 
 end
